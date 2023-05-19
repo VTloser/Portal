74a5d2297825c90c01f4c557e6703b4125f4d3dc
VTloser <39976734+VTloser@users.noreply.github.com>
2023-05-19 17:31:59 +0800

    1

---
+++ CC.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CC : MonoBehaviour
+{
+
+    public Transform Player_Door;
+    public Transform PlayCam;
+
+
+    public Transform Portal_Door;
+    public Transform Portal_Cam;
+
+
+    public MeshRenderer Player_scene;
+
+    public RenderTexture viewTexture;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Matrix4x4 m = Player_Door.worldToLocalMatrix * Portal_Door.localToWorldMatrix * PlayCam.localToWorldMatrix;
+        Portal_Cam.SetPositionAndRotation(m.GetColumn(3), m.rotation);
+
+
+        if (viewTexture == null || viewTexture.width != Screen.width || viewTexture.height != Screen.height)
+        {
+            if (viewTexture != null)
+            {
+                viewTexture.Release();
+            }
+            //viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+
+
+        }
+
+
+        Portal_Cam.GetComponent<Camera>().targetTexture = viewTexture;
+
+        Player_scene.material.SetTexture("_MainTex", viewTexture);
+
+
+    }
+}
---
+++ Portal.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal LinkedPortal;
+
+    public MeshRenderer screen;
+
+    [SerializeField] Camera playerCam;
+
+    [SerializeField] Camera portalCam;
+
+    RenderTexture viewTexture;
+
+    void Start()
+    {
+        playerCam = Camera.main;
+        portalCam = GetComponentInChildren<Camera>();
+
+
+        portalCam.enabled = false;
+    }
+
+    void CreateViewTexture()
+    {
+        if (viewTexture == null || viewTexture.width != Screen.width || viewTexture.height != Screen.height)
+        {
+            if (viewTexture != null)
+            {
+                viewTexture.Release();
+            }
+            viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+
+            portalCam.targetTexture = viewTexture;
+
+            LinkedPortal.screen.material.SetTexture("_MainTex", viewTexture);
+        }
+    }
+
+    // 仅仅在Player Camera 在渲染的时候调用
+    public void Render()
+    {
+        //screen.enabled = false;
+
+        CreateViewTexture();
+
+        var m = transform.localToWorldMatrix * LinkedPortal.transform.localToWorldMatrix * playerCam.transform.localToWorldMatrix;
+        portalCam.transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
+
+        //Render the Camrea
+        portalCam.Render();
+
+        portalCam.enabled = true;
+
+    }
+    
+}
